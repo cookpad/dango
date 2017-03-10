236a3e9596f6e3eff2518f34e3cf807a9dfe1b1f
Matthew Mongeau <halogenandtoast@gmail.com>
2017-03-10 16:35:55 +0900

    Add setup script

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :dango, Dango.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "dango_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :dango, Dango.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "dango_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
